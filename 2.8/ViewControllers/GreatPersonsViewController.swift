570253e3582e6996fb80afe05f9fec279b0bb6c6
JohnyLearnSwift <eugenietyan@gmail.com>
2022-08-20 15:56:49 +0300

    tableView MVP

--- 2.8/ViewControllers/GreatPersonsViewController.swift
+++ 2.8/ViewControllers/GreatPersonsViewController.swift
@@ -13,32 +13,47 @@ class GreatPersonsViewController: UITableViewController {
 
     var discipline: Discipline!
     
+    private var greatPersons: [[GreatPerson]] = []
+    // MARK: - Позже додумать как удалиться от хардкодинга, экстеншин или гет добавить к енаму
+    private let sectionNames = ["Философия", "Математика", "Физика"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.rowHeight = 80
+        getInfoFromDataStorage()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("\(self): \(discipline.rawValue)")
     }
 
     // MARK: - Table view data source
 
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         Discipline.allCases.count
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let discipline = greatPersons[section]
+        return discipline.count
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        sectionNames[section]
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "greatPerson", for: indexPath)
-        // Добавить фотографии и имена известных людей
-//        var content = cell.defaultContentConfiguration()
+        var content = cell.defaultContentConfiguration()
+        let discipline = greatPersons[indexPath.section]
+        let greatPerson = discipline[indexPath.row]
         
-//        let contact = Discipline.allCases[indexPath.row]
-//
-//        content.text = contact.rawValue
-//        cell.contentConfiguration = content
+        content.text = greatPerson.name
+        content.image = UIImage(named: greatPerson.image)
+        content.imageProperties.cornerRadius = tableView.rowHeight / 2
+        content.imageProperties.maximumSize.height = tableView.rowHeight
+        content.imageProperties.maximumSize.width = tableView.rowHeight
+        
+        cell.contentConfiguration = content
         return cell
     }
     
@@ -47,5 +62,11 @@ class GreatPersonsViewController: UITableViewController {
 extension GreatPersonsViewController: GreatPersonDelegate {
     func setDiscipline(_ newDiscipline: Discipline) {
      discipline = newDiscipline
+    }
+    
+    private func getInfoFromDataStorage() {
+        greatPersons.append(GreatPersonDataManager.shared.philosophers)
+        greatPersons.append(GreatPersonDataManager.shared.mathematicians)
+        greatPersons.append(GreatPersonDataManager.shared.physicians)
     }
 }
